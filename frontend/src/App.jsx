6ebba60e397601570fbe44732849f9f6350ec086
Shubham <shubhamkumarsingh424@gmial.com>
2025-01-30 15:21:25 +0530

    fixed app.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import { Routes,Route} from 'react-router-dom'
 import Home from './pages/Home'
-import Collection from './pages/collection'
+import Collection from './pages/Collection'
 import Cart from './pages/Cart'
 import Contact from './pages/Contact'
 import Login from './pages/Login'
